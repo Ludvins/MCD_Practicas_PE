ace2f1b062a9f35b60c1e9899908c86dfc923f45
antcc <antoniocoin07@gmail.com>
2020-12-04 14:09:42 +0100

    Finish ex 2

--- src/1.py
+++ src/1.py
@@ -1,24 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import poisson, expon, uniform, gaussian_kde
-
-def simulate_poisson_process_v1(rate, t0, t1):
-    # Leverage that interarrival times follow an exponential(rate)
-    ss = [t0 + expon.rvs(scale = 1.0 / rate)]
-    while ss[-1] < t1:
-        ss.append(ss[-1] + expon.rvs(scale = 1.0 / rate))
-
-    return ss
-
-def simulate_poisson_process_v2(rate, t0, t1):
-    # Sample 'n' from a Poisson distribution
-    n = poisson.rvs((t1 - t0) * rate)
-
-    # Generate n independen samples from U[t0, t1]
-    u = uniform.rvs(loc = t0, scale = t1 - t0, size = n)
-
-    # Order the samples and interpret them as time events
-    return np.sort(u)
+from scipy.stats import poisson, gaussian_kde
+from arrival import simulate_poisson_process_v1, simulate_poisson_process_v2
 
 # Process parameters
 rate = 10
@@ -29,26 +12,31 @@ ns = np.arange(50)
 pmf = poisson.pmf(ns, rate * t)
 
 # Simulation
-max_sims = int(1e4)
-events_v1 = np.array([len(simulate_poisson_process_v1(rate, 0, t)) for _ in range(max_sims)])
-density_estimate_v1 = gaussian_kde(events_v1)
-events_v2 = np.array([len(simulate_poisson_process_v2(rate, 0, t)) for _ in range(max_sims)])
-density_estimate_v2 = gaussian_kde(events_v2)
+M = int(1e4)
+events_v1 = simulate_poisson_process_v1(rate, 0, t, M)
+counts_v1 = [len(events_v1[m]) for m in range(M)]
+density_estimate_v1 = gaussian_kde(counts_v1)
+events_v2 = simulate_poisson_process_v2(rate, 0, t, M)
+counts_v2 = [len(events_v2[m]) for m in range(M)]
+density_estimate_v2 = gaussian_kde(counts_v2)
+counts = [counts_v1, counts_v2]
+density_estimates = [density_estimate_v1, density_estimate_v2]
 
 # Plot results to compare
 fig, axs = plt.subplots(1, 3, sharey = True)
 axs[0].stem(ns, pmf)
 axs[0].set_title(r"$P[N(2)=n] = \frac{1}{n!} (20)^n e^{-20}$")
+axs[0].set_xlabel("Number of events")
+axs[0].set_ylabel("Density")
 
-axs[1].hist(events_v1, density = True, bins = 25, edgecolor = 'k')
-axs[1].plot(ns, density_estimate_v1(ns), label = "Estimated density")
-axs[1].plot(ns, poisson.pmf(ns, rate * t), label = "True 'density'")
-axs[1].set_title(r"Simulation (v1) of Poisson process in $[0, 2]$ with rate $\lambda = 10$")
-axs[1].legend()
-
-axs[2].hist(events_v2, density = True, bins = 25, edgecolor = 'k')
-axs[2].plot(ns, density_estimate_v2(ns), label = "Estimated density")
-axs[2].plot(ns, poisson.pmf(ns, rate * t), label = "True 'density'")
-axs[2].set_title(r"Simulation (v2) of Poisson process in $[0, 2]$ with rate $\lambda = 10$")
-axs[2].legend()
+for i, (count, density_estimate) in enumerate(zip(counts, density_estimates)):
+    axs[i + 1].hist(count, density = True, bins = 25, edgecolor = 'k')
+    axs[i + 1].plot(ns, poisson.pmf(ns, rate * t), label = "True 'density'",
+                color = "red", lw = 2)
+    axs[i + 1].plot(ns, density_estimate(ns), "--", label = "Estimated density",
+                lw = 2, color = "gold")
+    axs[i + 1].set_title(f"Simulation (v{i+1}) "
+                + r"of a Poisson process in $[0, 2]$ with rate $\lambda = 10$")
+    axs[i + 1].legend()
+    axs[i + 1].set_xlabel("Number of events")
 plt.show()
--- src/2.py
+++ src/2.py
@@ -1,10 +1,35 @@
-#!/usr/bin/env python3
-
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde, erlang
+from arrival import simulate_poisson_process_v2
 
-def poisson_process(_lambda, t0, t1):
-    ts = [t0]
-    while ts[-1] < t1:
-        ts.append(ts[-1] + np.random.exponential(1/_lambda))
+# Process parameters
+rate = 5
+beta = 1.0 / rate
+ns = [1, 2, 5, 10]
 
-    return ts[1:-1]
+# Theoretical PDF
+ts = np.arange(0, 6, 0.01)
+pdfs = [erlang.pdf(ts, a = n, scale = beta) for n in ns]
+
+# Simulation
+M = int(1e4)
+events = simulate_poisson_process_v2(rate, 0, ts[-1], M)
+events = [[events[m][n - 1] for m in range(M)] for n in ns]
+density_estimates = [gaussian_kde(events[n]) for n in range(len(ns))]
+
+# Plot results to compare
+fig, axs = plt.subplots(2, 2)
+
+for i in [0, 1]:
+    for j in [0, 1]:
+        n = 2 * i + j
+        axs[i, j].hist(events[n], label = f"Simulated S{ns[n]}", density = True, edgecolor = 'k', bins = 20 + n)
+        axs[i, j].plot(ts, pdfs[n], label = f"True density of S{ns[n]}",
+                       color = "red", lw = 2)
+        axs[i, j].plot(ts, density_estimates[n](ts), "--", label = f"Estimated density of S{ns[n]}", lw = 2, color = "gold")
+        axs[i, j].set_title(f"Distribution of event #{ns[n]}")
+        axs[i, j].set_xlabel("Time")
+        axs[i, j].set_ylabel("Density")
+        axs[i, j].legend()
+plt.show()
